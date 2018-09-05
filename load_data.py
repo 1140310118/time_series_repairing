d9d3d2f2db8c9111507e1e011a92e50c289e1382
luciferldy <2460572865@qq.com>
2018-09-05 14:12:01 +0800

    添加数据加载部分

---
+++ load_data.py
@@ -0,0 +1,72 @@
+import copy
+import torch
+import torch.utils.data as Data
+import numpy as np
+
+
+def make_data(data,time_step,batch_size,dimension,missing_rate=0.2):
+	"""
+	将时间序列进行缺失处理，返回
+	  - X 缺失的时间序列及缺失的位置
+	  - Y 完整的时间序列
+	"""
+	Y = data.reshape(batch_size,time_step,dimension)
+	Y_copy = copy.deepcopy(Y)
+
+	X = []
+	masks = []
+	for i in range(time_step):
+		if not X: # 首次循环，不缺失
+			x = Y_copy[:,0]
+			mask = np.ones_like(x)
+		else:
+			x = Y_copy[:,i]
+			mask = (np.random.random(x.shape) > missing_rate).astype(np.int32)
+			x = x*mask + X[-1]*(1-mask)
+		masks.append(mask)	
+		X.append(x)
+
+	masks = np.array(masks)
+	X = np.array(X)
+	X = np.concatenate((X,masks),axis=2)
+	X = np.swapaxes(X,0,1) # 交换前两个维度，使得矩阵的形状变为 (batch_size,time_step,dimension) 
+	
+	return X,Y
+
+def make_dataloader(X,Y,batch_size):
+	X,Y = torch.FloatTensor(X),torch.FloatTensor(Y)
+	torch_dataset = Data.TensorDataset(X,Y)
+	dataloader = Data.DataLoader(
+		dataset=torch_dataset,
+		batch_size=batch_size,
+		shuffle=True,
+		num_workers=2,
+	)
+	return dataloader
+
+def get_batch(np_data,time_step,batch_size,dimension,missing_rate):
+	"""
+	返回一个dataloader
+	for b_x,b_y in dataloader:
+		b_x (batch_size,time_step,dimension*2)
+		b_y (batch_size,time_step,dimension)
+	"""
+	num = np_data.shape[0]
+	X,Y = make_data(np_data,time_step,int(num/time_step),dimension,missing_rate)
+	dataloader = make_dataloader(X,Y,batch_size)
+	return dataloader
+
+
+if __name__ == "__main__":
+	data_train = np.load('ts_data/data_train.npy')
+	data_valid = np.load('ts_data/data_valid.npy')
+	
+	TIME_STEP  = 10
+	BATCH_SIZE = 100
+	dimension  = 66
+	missing_rate = 0.2
+	
+	train_dataloader = get_batch(data_train,TIME_STEP,BATCH_SIZE,dimension,missing_rate)
+	valid_dataloader = get_batch(data_valid,TIME_STEP,BATCH_SIZE,dimension,missing_rate)
+	
+
