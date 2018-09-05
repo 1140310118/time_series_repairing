3a38d60a966d9f3831bc37115f97e8032da5c44a
luciferldy <2460572865@qq.com>
2018-09-06 00:00:58 +0800

    gru超参数调节 1

--- gru.py
+++ gru.py
@@ -18,59 +18,81 @@ class RNN(nn.Module):
 	def __init__(self):
 		super(RNN,self).__init__()
 		self.rnn = nn.GRU(
-			input_size  = DIMENSION*2,
+			input_size  = DIMENSION,
 			hidden_size = 256,
 			num_layers  = 2,
-			batch_first = True,
-			dropout     = 0.5,
+			batch_first = True, # batch_size,time_step,dimension
+			dropout     = 0.05,
 		)
+		# self.bn = nn.BatchNorm1d(256)
 		self.out = nn.Linear(256,DIMENSION)
 
 	def forward(self,x,h_state):
 		r_out,h_state = self.rnn(x,h_state)
+		# r_out = self.bn(r_out)
 		return self.out(r_out),h_state
+
+
+class MSELoss_with_mask(nn.Module):
+
+	def __init__(self,weight=1):
+		super(MSELoss_with_mask,self).__init__()
+		self.weight = weight
+	
+	def forward(self,y_predict,y_target,mask):
+		"""
+		weight 缺失值部分所占的额外权重
+		"""
+		one_num  = torch.sum(mask)
+		zero_num = torch.sum(1-mask)
+		mse  = nn.MSELoss()
+		loss1 = mse(y_predict*(1-mask),
+					y_target *(1-mask)) *zero_num/(zero_num+one_num)
+		loss2 = mse(y_predict, y_target)
+		loss = (loss1*self.weight+loss2)/(1+self.weight)
+		return loss
 
 
 def get_loss(rnn,loader):
 	sum_loss = 0
 	count = 0
-	loss_func = nn.MSELoss()
-	for step,(b_x,b_y) in enumerate(loader):
-		b_x = b_x.cuda()
-		b_y = b_y.cuda()
+	loss_func = MSELoss_with_mask()
+	for step,(b_x,b_y,mask) in enumerate(loader):
+		b_x  = b_x.cuda()
+		b_y  = b_y.cuda()
+		mask = mask.cuda()
 		prediction,_ = rnn(b_x,None)
-		loss = loss_func(prediction,b_y)
-		sum_loss += loss
+		loss = loss_func(prediction,b_y,mask)
+		sum_loss += float(loss)
 		count += 1
 	mseLoss = sum_loss/count
-	return mseLoss.item()
+	return mseLoss
 
 
 def train(rnn,loader,loader2):
 	optimizer = torch.optim.Adam(rnn.parameters(),lr=LR)
-	loss_func = nn.MSELoss()
+	loss_func = MSELoss_with_mask()
 
-	for epoch in range(100):
-		for step,(b_x,b_y) in enumerate(loader):
+	for epoch in range(250):
+		for step,(b_x,b_y,mask) in enumerate(loader):
 			b_x = b_x.cuda()
 			b_y = b_y.cuda()
+			mask = mask.cuda()
 
 			prediction,_ = rnn(b_x,None)
-			loss = loss_func(prediction,b_y)
+			loss = loss_func(prediction,b_y,mask)
 
 			optimizer.zero_grad()
 			loss.backward()
 			optimizer.step()
 
-			if (step+1)%100 == 0:
-				print (epoch,step+1,"%.4f"%loss.item(),end=" ")
-				print ("%.4f"%get_loss(rnn,loader2))
+		print (epoch+1,step+1,"%.5f"%loss.item(),end=" ")
+		print ("%.5f"%get_loss(rnn,loader2))
 
 
 if __name__ == "__main__":
 	data_train = np.load('ts_data/data_train.npy')
-	data_valid = np.load('ts_data/data_valid.npy')[:10000]
-	
+	data_valid = np.load('ts_data/data_valid.npy')
 	TIME_STEP  = 10
 	BATCH_SIZE = 100
 	dimension  = 66
@@ -81,5 +103,6 @@ if __name__ == "__main__":
 
 	rnn = RNN()
 	rnn.cuda()
+	print(rnn)
 
 	train(rnn, train_dataloader, valid_dataloader)
--- load_data.py
+++ load_data.py
@@ -27,15 +27,16 @@ def make_data(data,time_step,batch_size,dimension,missing_rate=0.2):
 		X.append(x)
 
 	masks = np.array(masks)
+	masks = np.swapaxes(masks,0,1) # 交换前两个维度，使得矩阵的形状变为 (batch_size,time_step,dimension) 
 	X = np.array(X)
-	X = np.concatenate((X,masks),axis=2)
-	X = np.swapaxes(X,0,1) # 交换前两个维度，使得矩阵的形状变为 (batch_size,time_step,dimension) 
+	X = np.swapaxes(X,0,1) # 同上
+	# X = np.concatenate((X,masks),axis=2)
 	
-	return X,Y
+	return X,Y,masks
 
-def make_dataloader(X,Y,batch_size):
-	X,Y = torch.FloatTensor(X),torch.FloatTensor(Y)
-	torch_dataset = Data.TensorDataset(X,Y)
+def make_dataloader(X,Y,masks,batch_size):
+	X,Y,masks = torch.FloatTensor(X),torch.FloatTensor(Y),torch.FloatTensor(masks)
+	torch_dataset = Data.TensorDataset(X,Y,masks)
 	dataloader = Data.DataLoader(
 		dataset=torch_dataset,
 		batch_size=batch_size,
@@ -52,8 +53,8 @@ def get_batch(np_data,time_step,batch_size,dimension,missing_rate):
 		b_y (batch_size,time_step,dimension)
 	"""
 	num = np_data.shape[0]
-	X,Y = make_data(np_data,time_step,int(num/time_step),dimension,missing_rate)
-	dataloader = make_dataloader(X,Y,batch_size)
+	X,Y,masks = make_data(np_data,time_step,int(num/time_step),dimension,missing_rate)
+	dataloader = make_dataloader(X,Y,masks,batch_size)
 	return dataloader
 
 
@@ -68,5 +69,6 @@ if __name__ == "__main__":
 	
 	train_dataloader = get_batch(data_train,TIME_STEP,BATCH_SIZE,dimension,missing_rate)
 	valid_dataloader = get_batch(data_valid,TIME_STEP,BATCH_SIZE,dimension,missing_rate)
+	
 	
 
